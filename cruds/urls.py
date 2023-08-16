95833a126e2d5c8e9a97c204490193301e58ea84
raihanratul2 <raihanratul25044@gmail.com>
2023-08-16 22:11:51 +0600

    modified:   CONFIG/urls.py 	modified:   cruds/admin.py 	modified:   cruds/models.py 	modified:   cruds/views.py 	modified:   db.sqlite3     modified:   templates/signup.html

--- cruds/urls.py
+++ cruds/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('',home,name='home',),
-    path('endgame',endgame,name='endgame',),
-    path('signup',signup,name='signup',),
-    path('signin',signin,name='signin',),
+    path('endgame/',endgame,name='endgame',),
+    path('signup/',signup,name='signup',),
+    path('signin/',signin,name='signin',),
 ]
